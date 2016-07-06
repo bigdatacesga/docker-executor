5f4773fb7a3e510a25d52fbb0a35f8b0cd22cf39
Javier Cacheiro <jlopez@cesga.es>
2016-07-06 16:09:17 +0200

    clean up pipework device on stop

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='docker-executor',
-    version='0.2.2',
+    version='0.3.0',
     author='Javier Cacheiro',
     author_email='bigdata-dev@listas.cesga.es',
     url='https://github.com/javicacheiro/docker-executor',
