de0d6c367c87b4f6a1012f51a6c23d88685e3dd8
Javier Cacheiro <jlopez@cesga.es>
2016-07-15 12:14:43 +0200

    workaround for docker ps zombie processes

--- executor/utils.py
+++ executor/utils.py
@@ -12,9 +12,10 @@ def run(cmd):
 def wait(container_name):
     """Wait until container is running"""
     logging.info('Verifying if container {} is running'.format(container_name))
+    time.sleep(2)
     while is_not_running(container_name):
         logging.debug('Waiting for container {} to start'.format(container_name))
-        time.sleep(2)
+        time.sleep(5)
     logging.info('Container {} is running'.format(container_name))
 
 
@@ -25,8 +26,11 @@ def is_not_running(container_name):
 
 def is_running(container_name):
     """Check if a container is running"""
-    show_cmd = ['docker', 'ps', '-q', '--filter', 'name={}'.format(container_name)]
-    found = subprocess.check_output(show_cmd)
-    if found:
+    #show_cmd = ['docker', 'ps', '-q', '--filter', 'name={}'.format(container_name)]
+    #found = subprocess.check_output(show_cmd)
+    # docker top <name> exit status is 1 if the container is not running
+    top_cmd = ['docker', 'top', container_name]
+    exit_status = subprocess.call(top_cmd)
+    if exit_status == 0:
         return True
     return False
